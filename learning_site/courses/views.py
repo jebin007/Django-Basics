9cb7ee8054bd7ff76dd7e0108de8030040af53a9
jebin007 <jevinsubedi@gmail.com>
2017-06-28 15:37:28 -0500

    Added a detail view for Step model.
    Notes: linebreaks is a filter that turns two returns in a row into HTML paragraph tags.
    
    You apply filters to a variable with the pipe (|) character.

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Course
+from .models import Course, Step
 
 
 def course_list(request):
@@ -11,3 +11,7 @@ def course_detail(request, pk):
     #course = Course.objects.get(pk=pk)
     course = get_object_or_404(Course, pk=pk) #This line adds a 404 error if no objetcs are available instead of standard django 505 error like above
     return render (request, 'courses/course_detail.html', {'course': course})
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'courses/step_detail.html' , {'step': step})
