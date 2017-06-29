d70c911d4fa2f6e45033a3c8cf6c0a4a8a3ea445
jebin007 <jevinsubedi@gmail.com>
2017-06-29 03:28:57 -0500

    Created unit test for:
    1.CourseModeltest:  self.assertAlmostEqual(course.created_at, now) (previously created)
    
    2.StepModelTest : self.assertIn(step, self.course.step_set.all())
    
    3.CourseViewsTests : self.assertEqual(resp.status_code, 200)
                         self.assertIn(self.course, resp.context['courses'])
                         self.assertIn(self.course2, resp.context['courses'])
    Notes:
    django.core.urlresolvers.reverse() takes a URL name and reverses it to the correct URL string. More information
    
    self.client acts like a web browser and lets you make requests to URLs, both inside and outside of your Django project.
    
    assertEqual(a, b) checks that a and b are equal to each other.
    
    assertIn(a, b) checks that a is contained in b.

--- learning_site/courses/tests.py
+++ learning_site/courses/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
-from .models import Course
+from .models import Course, Step
 
 class CourseModelTests(TestCase):
     def test_course_creation(self):
@@ -11,4 +12,39 @@ class CourseModelTests(TestCase):
             description = "Learn to write regular expressions in Python"
         )
         now = timezone.now()
-        self.assertLess(course.created_at, now)
+        self.assertAlmostEqual(course.created_at, now)
+
+class StepModelTests(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title="Python Testing",
+            description="Learn to write tests in Python"
+        )
+
+    def test_step_creation(self):
+        step = Step.objects.create(
+            title="Introduction to Doctests",
+            description="Learn to write tests in your docstrings.",
+            course=self.course
+        )
+        self.assertIn(step, self.course.step_set.all())
+class CourseViewsTests(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title="Python Testing",
+            description="Learn to write tests in Python"
+        )
+        self.course2 = Course.objects.create(
+            title="New Course",
+            description="A new course"
+        )
+        self.step = Step.objects.create(
+            title="Introduction to Doctests",
+            description="Learn to write tests in your docstrings.",
+            course=self.course
+        )
+    def test_course_list_view(self):
+        resp = self.client.get(reverse('courses:list'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(self.course, resp.context['courses'])
+        self.assertIn(self.course2, resp.context['courses'])
